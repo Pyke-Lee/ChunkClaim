83801ee0f26006016ff97c982a7513edbb0483da
Pyke Lee <lsh069283@gmail.com>
2025-07-29 01:13:28 +0900

    ChunkClaim

--- src/main/java/io/github/pyke/chunkclaim/ChunkClaim.java
+++ src/main/java/io/github/pyke/chunkclaim/ChunkClaim.java
@@ -1,20 +1,32 @@
 package io.github.pyke.chunkclaim;
 
+import io.github.pyke.chunkclaim.command.ChunkClaimCommand;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ChunkClaim extends JavaPlugin {
+    public static final Component SYSTEM_PREFIX = Component.text("[SYSTEM] ").color(TextColor.color(0xffaa00));
     public static final Logger LOGGER = LogManager.getLogger("ChunkClaim");
+    private static Plugin instance;
 
     @Override
     public void onEnable() {
-        LOGGER.info("ChunkClaim 플러그인 활성화!");
+        instance = this;
+
+        new ChunkClaimCommand("ChunkClaim").register();
+
+        LOGGER.info("ChunkClaim 플러그인이 활성화되었습니다!");
     }
 
     @Override
     public void onDisable() {
-        LOGGER.info("ChunkClaim 플러그인 비활성화!");
+        LOGGER.info("ChunkClaim 플러그인이 비활성화되었습니다!");
     }
+
+    public static Plugin getInstance() { return instance; }
 }
